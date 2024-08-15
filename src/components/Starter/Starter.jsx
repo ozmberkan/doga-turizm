1a828375ee4e4213658ff51e0d42c5a540b9ed7d
ozmberkan <ozmberkan@gmail.com>
2024-08-15 15:13:31 +0300

    Responsive hatalar giderildi

--- src/components/Starter/Starter.jsx
+++ src/components/Starter/Starter.jsx
@@ -5,7 +5,7 @@ import { IoCall } from "react-icons/io5";
 const Starter = () => {
   return (
     <div className="sm:w-2/3 w-full gap-y-2 sm:gap-0 px-14 py-2 container sm:bg-[#f9f9f9] mx-auto flex justify-between items-center flex-col sm:flex-row">
-      <h1 className="font-rubik text-base font-medium">
+      <h1 className="font-rubik text-sm sm:text-base font-medium">
         Rüya gibi bir yolculuğa hazır olun!
       </h1>
       <div className="flex justify-center items-center gap-x-5">
