31ac37a955e8f7b5ec1071ec381f0bd46a2e2671
ozmberkan <ozmberkan@gmail.com>
2024-08-15 15:27:15 +0300

    announcement eklendi!

---
+++ src/components/Announcement/Announcement.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Banner from "../../assets/banner.png";
+
+const Announcement = () => {
+  return (
+    <div className="container w-full sm:w-2/3 h-full flex justify-center items-center z-20 mx-auto px-14 flex-col gap-y-2 bg-white rounded-md border my-5">
+      <div className="py-5 flex justify-center items-center font-rubik flex-col gap-y-2 w-full">
+        <h1 className="text-3xl">Doğa Turizm Şimdi Mobil Versiyonda!</h1>
+        <span className="text-sm text-zinc-700">
+          Yeni güncellemeyle beraber artık Doğa Turizm'den cep telefonlarınız
+          ile online rezervasyon yapabilirsiniz.
+        </span>
+        <div className="w-full  h-full">
+          <img
+            src={Banner}
+            alt=""
+            className="w-full rounded-md h-[350px]  object-cover shadow-xl mt-2"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Announcement;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Starter from "../components/Starter/Starter";
 import Campaign from "../components/Campaigns/Campaign";
+import Announcement from "../components/Announcement/Announcement";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Starter />
       <Header />
       <Campaign />
+      <Announcement />
     </div>
   );
 };
