18239f8fa78d8eb1ccd79b63c6ece7e483bc12d4
ozmberkan <ozmberkan@gmail.com>
2024-08-23 23:51:59 +0300

    tickets düzeltildi

--- src/pages/Tickets/TicketDetail.jsx
+++ src/pages/Tickets/TicketDetail.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { CiCalendarDate, CiLocationOn, CiClock2 } from "react-icons/ci";
+import { FaTurkishLiraSign, FaWifi } from "react-icons/fa6";
+import { FiTv } from "react-icons/fi";
+import { PiLightning } from "react-icons/pi";
 
 const TicketDetail = () => {
-  return <div>TicketDetail</div>;
+  const seats = Array.from({ length: 20 }, (_, i) => i + 1);
+  const [selectedSeat, setSelectedSeat] = useState(null);
+  return (
+    <div className="w-full rounded-xl p-5 flex gap-x-5 bg-[#E6F7E6]">
+      <div className="w-[400px] border bg-[#f0faf0] rounded-xl">
+        <div className="bg-white  p-3 rounded-t-xl">
+          <h1 className="text-2xl font-bold text-center text-[#4FC647]">
+            Bilet Detayı
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-green-800 p-5 h-[200px]">
+          <div className="flex items-center justify-center gap-x-1">
+            <CiLocationOn />
+            <span className="font-semibold">Kalkış:</span> İstanbul
+          </div>
+          <div className="flex items-center justify-center gap-x-1">
+            <CiLocationOn />
+            <span className="font-semibold">Varış:</span> Ankara
+          </div>
+          <div className="flex items-center justify-center gap-x-1">
+            <CiCalendarDate />
+            <span className="font-semibold">Tarih:</span> 01.07.2023
+          </div>
+          <div className="flex items-center justify-center gap-x-1">
+            <CiClock2 />
+            <span className="font-semibold">Saat:</span> 10:00
+          </div>
+          <div className="flex items-center justify-center gap-x-1">
+            <FaTurkishLiraSign />
+            <span className="font-semibold">Fiyat:</span> 150 TL
+          </div>
+          <div className="flex items-center justify-center gap-x-3">
+            <FaWifi />
+            <FiTv />
+            <PiLightning />
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#f0faf0] border rounded-xl flex flex-col justify-between">
+        <div className="bg-white p-3 rounded-t-xl">
+          <h1 className="text-2xl font-bold text-center text-[#4FC647]">
+            Koltuk Seçimi
+          </h1>
+        </div>
+        <div className="p-5 flex gap-3 flex-wrap justify-center">
+          {seats.map((seat) => (
+            <button
+              key={seat}
+              onClick={() => setSelectedSeat(seat)}
+              className={`h-10 w-10 rounded-xl border text-center ${
+                selectedSeat === seat
+                  ? "bg-green-600 border-green-600 text-white"
+                  : "bg-white text-green-600 border-green-300 hover:bg-green-50"
+              }`}
+            >
+              {seat}
+            </button>
+          ))}
+        </div>
+        <div className=" w-full p-4">
+          <button className="px-4 py-2 rounded-md text-white bg-[#4FC647] w-full">
+            Bileti Seç
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TicketDetail;
--- src/pages/Tickets/Tickets.jsx
+++ src/pages/Tickets/Tickets.jsx
@@ -1,65 +1,9 @@
-import React from "react";
-import { ConfigProvider, Steps, Collapse } from "antd";
-import { seatsData } from "~/data/data";
+import React, { useState } from "react";
+import { ConfigProvider, Steps } from "antd";
+
+import TicketDetail from "./TicketDetail";
 
 const Tickets = () => {
-  const TicketDetail = ({ label }) => {
-    return (
-      <div className="w-full flex justify-center items-center text-xl font-rubik gap-y-5 flex-col">
-        <div className="w-full">
-          <ul className="bg-[#4FC647] px-4 py-2 text-white rounded-md flex-1 flex justify-between gap-x-5 items-center w-full">
-            <span className="bg-[#4FC647] px-4 py-2 text-green-200 rounded-md">
-              {label}
-            </span>
-            <span className="flex gap-x-5 items-center ">
-              <li>Wifi</li>
-              <li>Tv</li>
-              <li>Yeni Otoban</li>
-              <li>Priz</li>
-              <li>Elektrikli Otobüs</li>
-            </span>
-            <li className="text-3xl">750₺</li>
-          </ul>
-        </div>
-        <div className="w-full">
-          <div className="w-full bg-red- p-24 grid grid-cols-10 gap-5">
-            {seatsData.map((seat) => (
-              <div
-                key={seat.id}
-                className={`w-full h-12 flex justify-center items-center rounded-md cursor-pointer ${
-                  seat.isAvailable ? "bg-green-300" : "bg-gray-500"
-                }`}
-              >
-                {seat.seatNumber}
-              </div>
-            ))}
-          </div>
-          <button className="bg-[#4FC647] px-4 py-2 text-white rounded-xl">
-            Bilet Al
-          </button>
-        </div>
-      </div>
-    );
-  };
-
-  const items = [
-    {
-      key: "1",
-      label: "13:00",
-      children: <TicketDetail label="13:00" />,
-    },
-    {
-      key: "2",
-      label: "14:00",
-      children: <TicketDetail label="14:00" />,
-    },
-    {
-      key: "3",
-      label: "15:00",
-      children: <TicketDetail label="15:00" />,
-    },
-  ];
-
   return (
     <div className="w-full h-screen container mx-auto p-7 flex flex-col gap-y-5 font-rubik">
       <ConfigProvider
@@ -86,26 +30,10 @@ const Tickets = () => {
           ]}
         />
       </ConfigProvider>
-      <div className="flex justify-center items-center p-5 rounded-md bg-white border w-full ">
-        <div className="flex justify-between items-center w-full text-xl">
-          <span>İstanbul {"->"} İzmir</span>
-          <span>21.08.2024</span>
-        </div>
-      </div>
-      <div className="w-full bg-white  rounded-md">
-        <ConfigProvider
-          theme={{
-            components: {
-              Collapse: {
-                contentBg: "#fff",
-                headerBg: "#f4f4f4",
-                colorBorder: "#E5E7EB",
-              },
-            },
-          }}
-        >
-          <Collapse size="large" items={items} defaultActiveKey={["1"]} />
-        </ConfigProvider>
+      <div className="flex justify-center items-center p-3 w-full flex-col gap-y-12">
+        <TicketDetail />
+        <TicketDetail />
+        <TicketDetail />
       </div>
     </div>
   );
