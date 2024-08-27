03d2ae5bd7d47f2c1b3d7c710ec64d585df09133
ozmberkan <ozmberkan@gmail.com>
2024-08-27 14:44:02 +0300

    Scrollbar Eklendi

--- src/components/Footer/FooterDetailBox.jsx
+++ src/components/Footer/FooterDetailBox.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./Footer";
 
 const FooterDetailBox = ({ children }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="w-full flex justify-center items-center mt-5 px-5 flex-col">
       <div className="bg-white rounded-md p-12 border w-full">{children}</div>
