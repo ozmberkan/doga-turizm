0fe1165aad033a9b53c7848115e568c888872b22
ozmberkan <ozmberkan@gmail.com>
2024-08-30 20:24:49 +0300

    Footer düzenlendi. Yeni tasarıma eklendi responsive düzeni yapıldı

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,14 +11,14 @@ import LogoBlack from "../../assets/logos/LogoBlack.png";
 import { Link } from "react-router-dom";
 
 const FooterSection = ({ title, items }) => (
-  <div className="flex flex-col gap-y-5 sm:items-start items-center py-5 sm:py-0 border-b sm:border-none">
+  <div className="flex flex-col gap-y-5 sm:items-start items-center py-5 w-full sm:w-auto sm:py-0 border-b sm:border-none">
     <h1 className="text-md underline">{title}</h1>
-    <ul className="flex flex-col items-start gap-y-2 w-full sm:w-0">
+    <ul className="flex flex-col items-start gap-y-2 w-full ">
       {items.map((item, i) => (
         <Link
           to={item.to}
           key={i}
-          className="p-2 text-sm rounded-md border w-full sm:min-w-[160px] flex gap-x-2 items-center cursor-pointer hover:bg-green-500 hover:text-white transition-colors duration-100"
+          className="p-2 text-sm bg-white rounded-md border w-full sm:min-w-[160px] flex gap-x-2 items-center cursor-pointer hover:bg-green-500 hover:text-white transition-colors duration-100"
         >
           {item.icon && <item.icon size={18} />}
           {item.title || item.name}
@@ -31,31 +31,25 @@ const FooterSection = ({ title, items }) => (
 const Footer = ({ footerWidth }) => {
   return (
     <div
-      className={`container w-full sm:${footerWidth} sm:mt-12 py-5 px-6 bg-white rounded-none sm:rounded-t-md border flex-col sm:flex-row grid grid-cols-1 sm:grid-cols-5 mx-auto gap-y-2 sm:my-0 my-5 font-rubik`}
+      className={`w-full  sm:bg-white rounded-t-xl border shadow-xl sm:${footerWidth} mx-auto p-5 flex flex-col gap-y-6 `}
     >
-      <div className="flex flex-col gap-y-5 sm:items-start items-center border-b py-5 sm:py-0 sm:border-none">
-        <img
-          src={LogoBlack}
-          className="w-[150px] sm:w-[200px] drop-shadow-2xl"
-        />
-        <button className="w-[160px]  flex justify-center items-center gap-x-2 p-1">
-          <BiDownload size={18} />
-          <span className="text-sm">Doğa Turizm İndir!</span>
-        </button>
-        <div className="flex flex-col gap-y-3">
-          {downloadImage.map((dImage, i) => (
-            <img
-              key={i}
-              src={dImage}
-              className="w-full sm:w-32 cursor-pointer hover:scale-105 transition-all duration-500"
-            />
-          ))}
+      <div className="sm:grid sm:grid-cols-5  flex flex-col justify-center items-center sm:place-items-start  p-5">
+        <div className="flex flex-col">
+          <img src={LogoBlack} alt="" className="w-44 mb-3" />
+          <div className="flex flex-col gap-y-3">
+            {downloadImage.map((dItem, i) => (
+              <img src={dItem} alt="" className="w-40" />
+            ))}
+          </div>
         </div>
+        <FooterSection title="Bizi Keşfet" items={footerAbout} />
+        <FooterSection title="Yardıma İhtiyacın Var Mı?" items={footerSSS} />
+        <FooterSection title="İş Ortağımız Ol" items={footerWith} />
+        <FooterSection title="Sosyal Medya" items={socialMedia} />
       </div>
-      <FooterSection title="Bizi Keşfet" items={footerAbout} />
-      <FooterSection title="Yardıma İhtiyacın Var Mı?" items={footerSSS} />
-      <FooterSection title="İş Ortağımız Ol" items={footerWith} />
-      <FooterSection title="Sosyal Medya" items={socialMedia} />
+      <div className="col-span-1 sm:col-span-5 text-left border-t  text-sm py-5">
+        © 2024 Doğa Turizm. All rights reserved.
+      </div>
     </div>
   );
 };
