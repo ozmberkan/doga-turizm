7cdfc68ab1dc6808ffeefa52d8e548c31343f110
ozmberkan <ozmberkan@gmail.com>
2024-10-11 11:49:18 +0300

    Düzenlemeler yapıldı.

--- src/pages/Invoice/Invoice.jsx
+++ src/pages/Invoice/Invoice.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Logo from "~/assets/logos/LogoBlack.png";
+import LogoLight from "~/assets/logos/LogoBlack.png";
+import LogoDark from "~/assets/logos/Logo.png";
 import { IoDownloadOutline } from "react-icons/io5";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
@@ -9,6 +10,7 @@ import html2canvas from "html2canvas";
 const Invoice = () => {
   const { id } = useParams();
   const { user } = useSelector((store) => store.user);
+  const { theme } = useSelector((store) => store.theme);
 
   const findTicket = user.ownedTickets.find((ticket) => ticket.ticketID === id);
 
@@ -40,10 +42,10 @@ const Invoice = () => {
 
   return (
     <div className="w-full h-screen items-center justify-center flex flex-col gap-y-2 px-4 sm:px-0">
-      <div className="w-full sm:w-2/3 py-2 flex justify-end items-center">
+      <div className="w-full sm:w-1/2 py-2 flex justify-end items-center">
         <button
           onClick={downloadPDF}
-          className="px-4 py-2 rounded-md border bg-white flex items-center gap-x-1"
+          className="px-4 py-2 rounded-md border bg-white  flex items-center gap-x-1 "
         >
           <IoDownloadOutline />
           Faturayı İndir
@@ -51,22 +53,25 @@ const Invoice = () => {
       </div>
 
       <div
-        className="p-4 rounded-md border w-full sm:w-2/3 bg-white overflow-hidden"
+        className="p-4 rounded-md border w-full sm:w-1/2 bg-white overflow-hidden  "
         id="pdf-content"
       >
-        <div className="w-full flex justify-between items-center border-b py-2">
-          <img src={Logo} className="w-32 sm:w-44" />
-          <h1 className="text-black text-xl sm:text-2xl sm:flex hidden">
+        <div className="w-full flex justify-between items-center border-b  py-2">
+          <img
+            src={theme === "dark" ? LogoDark : LogoLight}
+            className="w-32 sm:w-44"
+          />
+          <h1 className="text-black text-xl sm:text-2xl sm:flex hidden ">
             e-Fatura
           </h1>
         </div>
 
         <div className="w-full flex flex-col gap-y-2 py-5">
           <div className="w-full flex justify-between items-center">
-            <h1 className="font-semibold text-xl sm:text-2xl py-2">
+            <h1 className="font-semibold text-xl sm:text-2xl py-2 ">
               Bilet Bilgileri
             </h1>
-            <p className="text-xs sm:text-sm text-zinc-600">
+            <p className=" text-sm text-zinc-600">
               Fatura No:{" "}
               {findTicket.ticketID.slice(0, 7) +
                 "-" +
@@ -74,8 +79,8 @@ const Invoice = () => {
             </p>
           </div>
           <div className="overflow-x-auto w-full">
-            <table className="w-full text-xs sm:text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border ">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b">
+            <table className="w-full text-xs sm:text-sm text-left rtl:text-right text-gray-500 ">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
                 <tr>
                   <th scope="col" className="px-1 sm:px-2 py-3 text-xs">
                     Açıklama
@@ -92,16 +97,14 @@ const Invoice = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <tr className="bg-white border-b ">
                   <th
                     scope="row"
-                    className="px-1 sm:px-2 py-4 text-xs font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    className="px-1 sm:px-2 py-4 text-xs font-medium text-gray-900 whitespace-nowrap "
                   >
                     {findTicket.departure} - {findTicket.arrival} Otobüs Bileti
                   </th>
-                  <td className="px-1 sm:px-2 py-4 text-[7px]">
-                    {findTicket.ticketID}
-                  </td>
+                  <td className="px-1 sm:px-2 py-4 ">{findTicket.ticketID}</td>
                   <td className="px-1 sm:px-2 py-4 text-xs">
                     {findTicket.seats.length}
                   </td>
@@ -114,16 +117,16 @@ const Invoice = () => {
           </div>
 
           <div className="w-full py-2 flex justify-start items-start flex-col">
-            <span className="text-xs sm:text-sm text-zinc-600">
+            <span className="text-xs sm:text-sm text-zinc-600 ">
               Ödeme Yöntemi: Kredi Kartı
             </span>
-            <span className="text-xs sm:text-sm text-zinc-600">
+            <span className="text-xs sm:text-sm text-zinc-600 ">
               Son 4 Hane: **** **** **** 1234
             </span>
           </div>
 
           <div className="w-full py-2 flex justify-start items-start flex-col">
-            <span className="text-xs sm:text-sm text-zinc-600">
+            <span className="text-xs sm:text-sm text-zinc-600 ">
               Bu fatura elektronik olarak oluşturulmuştur ve geçerli bir yasal
               belgedir. Sorularınız için lütfen müşteri hizmetleri ile iletişime
               geçin: dogaturizm@destek.com
